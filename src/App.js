309595d5ce895df7addc1354153818c29c47e2a9
khurshid idrees <umarkhurshid3@gmail.com>
2024-07-15 22:31:35 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 import Quize from './components/Quize';
-
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchDataAsync } from './redux/questionSlice';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(fetchDataAsync())
+  },[dispatch])
+
   return (
     <>
     <Quize/>
